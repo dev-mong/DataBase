5ce4f4a7f9a39199a402fbc897215a71b015e135
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-26 18:15:15 +0900

    PhoneBook DB

--- SQL_Study/PhoneBook/PHONEBOOK.sql
+++ SQL_Study/PhoneBook/PHONEBOOK.sql
@@ -4,7 +4,7 @@
 -----------------------
 --대리키 : 일련번호 -> p_idx
 
---이름, 전화번호, 생일 , 이메일
+--기본 정보 테이블 생성 
 create table phoneInfo_basic(
     p_idx number(6) constraint p_idx_pk primary key,
     fr_name varchar2(10) constraint fr_name_mm not null,
@@ -15,16 +15,17 @@ create table phoneInfo_basic(
 );
 
 -- 기본 정보 입력
-insert into phoneInfo_basic values(1,'박지성','010-1234-4578','서울','park@','86/05/05');
-insert into phoneInfo_basic values(2,'손흥민','010-1832-4578','인천','son@','89/12/26');
-insert into phoneInfo_basic values(3,'기성용','010-1234-0862','부산','park@','90/09/13');
-insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber, fr_regdate) 
-values(4,'이승우','010-6570-4039','98/06/07');
+insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber,fr_address,fr_email) values(1,'박지성','010-1234-4578','서울','park@');
+insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber,fr_address,fr_email) values(2,'손흥민','010-1832-4578','인천','son@');
+insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber,fr_address,fr_email)values(3,'기성용','010-1234-0862','부산','park@');
+insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber) 
+values(4,'이승우','010-6570-4039');
 select * from phoneInfo_basic;
 
 
 
--- 전공, 학년
+
+-- 대학 친구 테이블 생성
 create table phoneInfo_univ(
     idx number(6),
     fr_u_major varchar2(20) default 'N'  constraint fr_u_major_mm not null ,
@@ -42,7 +43,7 @@ select * from phoneInfo_univ u, phoneInfo_basic b where u.fr_ref=b.p_idx and b.p
 
 
 
--- 부서이름, 직급
+-- 회사 친구 테이블 생성
 create table phoneInfo_com(
     idx number(6),
     fr_c_company varchar2(20) default 'N' constraint fr_c_company_mm not null,
@@ -53,6 +54,8 @@ create table phoneInfo_com(
 --회사 친구 정보 입력
 insert into phoneInfo_com values(1,'bit',3);
 select * from phoneInfo_com c, phoneInfo_basic b where c.fr_ref=b.p_idx;
+
+
 
 select * from phoneInfo_basic b, phoneInfo_univ u, phoneInfo_com c where b.p_idx=c.fr_ref(+) and b.p_idx=u.fr_ref(+) order by p_idx;
 select * from phoneInfo_basic b, phoneInfo_univ u, phoneInfo_com c where b.p_idx=c.fr_ref and b.p_idx=u.fr_ref order by p_idx; -- 출력 x
