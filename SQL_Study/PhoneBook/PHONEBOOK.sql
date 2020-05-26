3b21e9c2ca1d7a3b41eefecb7a4ef7b8b9bf763d
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-05-26 17:41:30 +0900

    PhoneBook DB

--- SQL_Study/PhoneBook/PHONEBOOK.sql
+++ SQL_Study/PhoneBook/PHONEBOOK.sql
@@ -14,20 +14,35 @@ create table phoneInfo_basic(
     fr_regdate date default sysdate
 );
 
+-- 기본 정보 입력
+insert into phoneInfo_basic values(1,'박지성','010-1234-4578','서울','park@','86/05/05');
+insert into phoneInfo_basic values(2,'손흥민','010-1832-4578','인천','son@','89/12/26');
+insert into phoneInfo_basic values(3,'기성용','010-1234-0862','부산','park@','90/09/13');
+insert into phoneInfo_basic (p_idx, fr_name, fr_phonenumber, fr_regdate) 
+values(4,'이승우','010-6570-4039','98/06/07');
+select * from phoneInfo_basic;
+
 
 
 -- 전공, 학년
 create table phoneInfo_univ(
-    idx number(6) constraint univ_idx_pk primary key,
-    fr_u_major varchar2(20) default 'N'  constraint fr_u_major_pk not null ,
-    fr_u_year number(1) not null,
+    idx number(6),
+    fr_u_major varchar2(20) default 'N'  constraint fr_u_major_mm not null ,
+    fr_u_year number(1) default '1'constraint fr_u_year_mm  not null,
     fr_ref number(6),
+    constraint univ_idx_pk primary key(idx),
     constraint f_u_year_check check (fr_u_year between 1 and 4),
     constraint fr_ref_fk foreign key (fr_ref) references phoneInfo_basic(p_idx)
 );
+--대학친구 정보 입력
+insert into phoneInfo_univ values(1,'축구','2',2);
+insert into phoneInfo_univ (idx, fr_u_year, fr_ref) values(2,'3',4);
+select * from phoneInfo_univ u, phoneInfo_basic b where u.fr_ref=b.p_idx; 
+select * from phoneInfo_univ u, phoneInfo_basic b where u.fr_ref=b.p_idx and b.p_idx=3;  
 
 
---부서이름, 직급
+
+-- 부서이름, 직급
 create table phoneInfo_com(
     idx number(6),
     fr_c_company varchar2(20) default 'N' constraint fr_c_company_mm not null,
@@ -35,34 +50,9 @@ create table phoneInfo_com(
     constraint com_idx_pk primary key(idx),
     constraint fr_ref foreign key (fr_ref) references phoneInfo_basic(p_idx)
 );
+--회사 친구 정보 입력
+insert into phoneInfo_com values(1,'bit',3);
+select * from phoneInfo_com c, phoneInfo_basic b where c.fr_ref=b.p_idx;
 
-
-select * from tab;
---전화번호부 (contact)
-create table contact(
-    pidx number(6),
-    pname varchar2(10) constraint pname_mm not null,
-    phonenumber varchar2(20) constraint phonenumber_mm not null,
-    
-    address varchar2(30) default '입력값이 없습니다.',
-    email varchar2(20) default '입력값이 없습니다.',
-    major varchar2(20),
-    grade number(1), 
-    
-    comname varchar2(10),
-    dept varchar2(10),
-    
-    cafename varchar2(10),
-    nickname varchar2(10),
-    kategorie varchar2(5) not null, 
-    
-    constraint pidx_pk primary key(pidx),
-    constraint kategorie_check check (kategorie in ('univ','com','cafe')),
-    constraint grade_check check (grade between 1 and 4)
-);
-
-
-
-
-
-
+select * from phoneInfo_basic b, phoneInfo_univ u, phoneInfo_com c where b.p_idx=c.fr_ref(+) and b.p_idx=u.fr_ref(+) order by p_idx;
+select * from phoneInfo_basic b, phoneInfo_univ u, phoneInfo_com c where b.p_idx=c.fr_ref and b.p_idx=u.fr_ref order by p_idx; -- 출력 x
