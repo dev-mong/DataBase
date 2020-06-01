ba9b4aefe0a28bb2053a0ebc617cab8c343fd162
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-01 16:13:50 +0900

    사원, 부서 관리 ver0.1

---
+++ JDBC/JDBCDept/src/main/Main.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.Scanner;
+
+import dept.DeptManager;
+import emp.EmpManager;
+
+public class Main {
+
+	public static void main(String[] args)  {
+
+	Scanner sc=new Scanner(System.in);
+	
+	EmpManager mm=new EmpManager();
+	DeptManager dm=new DeptManager();
+	
+	try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	while(true) {
+		System.out.println("<< 메뉴 >>");
+		System.out.println("----------------------------");
+		System.out.println("1.사원 2.부서 0.종료");
+		System.out.println("----------------------------");
+		
+	
+		int menu=sc.nextInt();
+		sc.nextLine();
+		
+		switch(menu) {
+		case 1:
+			mm.menu();
+			break;
+		case 2: 
+			dm.menu();
+			break;
+		
+		case 0:
+			System.out.println("종료되었습니다.");
+			System.exit(0);
+		
+		}
+		}
+	}
+
+}
