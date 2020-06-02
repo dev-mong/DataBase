c0beed20b735d9b82499dfd0b16ae681d3c21cc5
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-02 23:57:31 +0900

    SQL 검색

--- JDBC/JDBCStudy/JDBCPhoneBook/src/phoneBook/PhoneInfoDao.java
+++ JDBC/JDBCStudy/JDBCPhoneBook/src/phoneBook/PhoneInfoDao.java
@@ -31,7 +31,7 @@ public class PhoneInfoDao {
 			
 			while(rs.next()) {
 				PhoneInfoDto pdto=new PhoneInfoDto(
-						rs.getInt("p_idx"),
+						rs.getInt("idx"),
 						rs.getString("fr_name"),
 						rs.getString("fr_phonenumber"),
 						rs.getString("fr_address"),
@@ -72,9 +72,69 @@ public class PhoneInfoDao {
 		return PhoneData;
 	}
 	
-	//전화번호 검색
-	public List<PhoneInfoDto> InfoName(){
+	//기본정보 검색
+	public List<PhoneInfoDto> searchName(String name){
 		
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+
+		List<PhoneInfoDto> phoneData=new ArrayList<>(); 
+		
+		try {
+			//DB 연결
+			conn=ConnectionProvider.getConnection();
+			
+			//sql 
+			String sql="select * from phoneInfo_basic where fr_name=?";
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1,name);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				PhoneInfoDto pdto=new PhoneInfoDto(
+						rs.getInt("idx"),
+						rs.getString("fr_name"),
+						rs.getString("fr_phonenumber"),
+						rs.getString("fr_address"),
+						rs.getString("fr_email"),
+						rs.getString("fr_regdate")
+						);
+				phoneData.add(pdto);
+			}else {
+				System.out.println("저장된 이름이 없습니다. 다시 입력하세요.");
+			}
+			
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//데이터 베이스 연결 종료
+			
+			if(rs !=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}if(pstmt !=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn !=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
 		
 		
 		return phoneData;
