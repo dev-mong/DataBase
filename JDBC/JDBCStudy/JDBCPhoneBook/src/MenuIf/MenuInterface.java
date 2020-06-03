c593e8b7921a6562ef0c86c9342af4866b4dc08e
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-03 21:27:28 +0900

    메뉴 인터페스

---
+++ JDBC/JDBCStudy/JDBCPhoneBook/src/MenuIf/MenuInterface.java
@@ -0,0 +1,12 @@
+package MenuIf;
+
+public interface MenuInterface {
+
+	public static final int menu1=1;
+	public static final int menu2=2;
+	public static final int menu3=3;
+	public static final int menu4=4;
+	public static final int menu5=5;
+	public static final int menu6=6;
+	public static final int menu0=0;
+}
