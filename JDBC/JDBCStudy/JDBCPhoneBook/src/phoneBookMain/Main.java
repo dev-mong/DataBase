827d2a5995eb059c7aff12d36cdbc92087e74d9f
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-03 22:13:42 +0900

    Main

---
+++ JDBC/JDBCStudy/JDBCPhoneBook/src/phoneBookMain/Main.java
@@ -0,0 +1,22 @@
+package phoneBookMain;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		//드라이버 로드
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+		PhoneInfoManager pm=new PhoneInfoManager();
+		pm.pmMenu();
+	}
+
+}
