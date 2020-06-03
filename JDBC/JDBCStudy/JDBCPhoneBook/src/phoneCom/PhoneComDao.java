23837fb419e099f9e23dba12788e3f87aef7046d
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-06-03 22:15:16 +0900

    Com Dao

--- JDBC/JDBCStudy/JDBCPhoneBook/src/phoneCom/PhoneComDao.java
+++ JDBC/JDBCStudy/JDBCPhoneBook/src/phoneCom/PhoneComDao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import phoneBook.ConnectionProvider;
+import phoneBookMain.ConnectionProvider;
 
 
 public class PhoneComDao {
